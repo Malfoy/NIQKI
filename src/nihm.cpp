72e437bdccd52b3dad11d202a9d8aaa11f3f77f6
BilbOk <cagret@mailo.com>
2021-10-14 11:25:12 +0200

    Update README

--- src/nihm.cpp
+++ src/nihm.cpp
@@ -1,14 +1,26 @@
 #include "nihm_index.h"
 #include "optionparser.h"
 
-#include <stdio.h>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
 #include <vector>
+#include <string>
+#include <fstream> //readline
+#include <iostream> //readline
+#include <csignal>
+#include <algorithm>
+
+#include <unistd.h>     // getpid(), getcwd()
+#include <sys/types.h>  // type definitions, e.g., pid_t
+#include <sys/stat.h>
+#include <sys/wait.h>   // wait()
+#include <sys/time.h>   // gettimeofday()
+#include <fcntl.h>
+#include <libgen.h>
+#include <signal.h>     // signal name constants and kill()
+
+#include <stdio.h>    
+#include <stdint.h>
 #include <atomic>
 #include <mutex>
-#include <stdint.h>
 #include <unordered_map>
 #include <pthread.h>
 #include <chrono>
@@ -70,17 +82,14 @@ const option::Descriptor usage[] = {
     "  --list, -l <file> "
       "\tUse the content of the given (raw formatted) <file> to load genomes.\v"
       "Example:"
-      "\v  --list my_genomes.lst" },
+      "\v  --list my_genomes.txt" },
   {QUERY_FILE, 0, "F", "query-file"    , Arg::NonEmpty,
     "  --query-file, -F <sequence_files> "
       "\tFor each sequence in the <sequence_files> search the sequence in the index and print the genomes"
       " with this sequence.\v"
       "The query file can be either a fasta formatted file (each sequence being a query) or a one line "
-      "raw query formatted file (following the same syntax as the query '--option'). In the latter case, "
-      "the only requirement is that the first character of the query file is different from '>' ; One "
-      "can also add comments starting with '#' until the end of line.\v"
       "Examples:"
-      "\v --query-file 'sequence_files'" },
+      "\v --query-file 'sequence_files.txt'" },
   {OUTPUT, 0, "o", "output", Arg::NonEmpty,
     "  --output, -o <filename> "
       "\tDump the current index in text format to the given file."
@@ -109,13 +118,21 @@ void deleteOptsArrays() {
 
 
 int main(int argc, char ** argv){
+
+cout << "I'm HERE 0" << endl;
   string filename = "";
+cout << "I'm HERE 1" << endl;
   argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
-  option::Stats  stats(true, usage, argc, argv);
+  option::Stats stats(true, usage, argc, argv);
+cout << "I'm HERE 2"  << endl;
   options = new option::Option[stats.options_max];
+cout << "I'm HERE 3" << endl;
   buffer = new option::Option[stats.buffer_max];
+cout << "I'm HERE 4" << endl;
   atexit(deleteOptsArrays);
+cout << "I'm HERE 5" << endl;
   option::Parser parse(usage, argc, argv, options, buffer);
+cout << "I'm HERE 6" << endl;
 
   if (parse.error()) {
     cout << "Bad usage!!!" << endl;
@@ -124,68 +141,69 @@ int main(int argc, char ** argv){
 
 
 
-/**********************************/
-/* Check Help and Version options */
-/**********************************/
-if (options[HELP] || argc == 0) {
-  return EXIT_SUCCESS;
-}
+  /**********************************/
+  /* Check Help and Version options */
+  /**********************************/
+  if (options[HELP] || argc == 0) {
+    return EXIT_SUCCESS;
+  }
 
 
-/************************************/
-/* Complain about unknown arguments */
-/************************************/
-for (int i = 0; i < parse.nonOptionsCount(); ++i) {
-  cout << "Non-option argument #" << i << " is " << parse.nonOption(i)<<endl;
-  cout << "Ignoring unknown argument '" << parse.nonOption(i) << "'" << endl;
-}
-if (parse.nonOptionsCount()) {
+  /************************************/
+  /* Complain about unknown arguments */
+  /************************************/
+  for (int i = 0; i < parse.nonOptionsCount(); ++i) {
+    cout << "Non-option argument #" << i << " is " << parse.nonOption(i)<<endl;
+    cout << "Ignoring unknown argument '" << parse.nonOption(i) << "'" << endl;
+  }
+  if (parse.nonOptionsCount()) {
     cout << "Bad usage!!!" << endl;
-  return EXIT_FAILURE;
-}
+    return EXIT_FAILURE;
+  }
 
-Index monidex(16,31,10,4);
-time_point<system_clock> start, endindex,end;
+  cout << "TODO ADD GENOOMES " << endl;
+  Index monidex(16,31,10,4);
+  time_point<system_clock> start, endindex,end;
 
-start = std::chrono::system_clock::now();
+  start = std::chrono::system_clock::now();
 
-/*****************************************/
-/* Add the genomes given in config files */
-/*****************************************/
-    if (options[LIST]) {
-        cout << "TODO ADD GENOOMES " << endl;
-    }
-    monidex.insert_file_of_file_whole("/home/bilbok/Documents/NiHM/Tools/dashing/fof.txt");
-    endindex = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = endindex - start;
-    std::cout << "Indexing lasted " << elapsed_seconds.count() << "s\n";
-    monidex.query_file_of_file_whole("/home/bilbok/Documents/NiHM/Tools/dashing/fof.txt");
-    end = std::chrono::system_clock::now();
-    elapsed_seconds =end-  endindex;
-    cout << "Query lasted " << elapsed_seconds.count() << "s\n";
-    elapsed_seconds =end-  start;
-    cout<<"whole run tool took " << elapsed_seconds.count() << endl;
+  /*****************************************/
+  /* Add the genomes given in config files */
+  /*****************************************/
+  if (options[LIST]) {
+    cout << "TODO ADD GENOOMES " << endl;
+  }
+  monidex.insert_file_of_file_whole("/home/bilbok/Documents/NiHM/Tools/dashing/fof.txt");
+  endindex = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = endindex - start;
+  std::cout << "Indexing lasted " << elapsed_seconds.count() << "s\n";
+  monidex.query_file_of_file_whole("/home/bilbok/Documents/NiHM/Tools/dashing/fof.txt");
+  end = std::chrono::system_clock::now();
+  elapsed_seconds =end-  endindex;
+  cout << "Query lasted " << elapsed_seconds.count() << "s\n";
+  elapsed_seconds =end-  start;
+  cout<<"whole run tool took " << elapsed_seconds.count() << endl;
 
 
 
-    /**********************************************/
-    /* Display the ASCII art logo of the program. */
-    /**********************************************/
-    if (options[LOGO_OPT]) {
-      ifstream logo ("../resources/nihm.ascii");
-      string line;
+  /**********************************************/
+  /* Display the ASCII art logo of the program. */
+  /**********************************************/
+  if (options[LOGO_OPT]) {
+    ifstream logo ("../resources/nihm.ascii");
+    string line;
 
-      if (logo.is_open()){
-        while ( getline (logo,line) ){
-          cout << line << '\n';
-        }
-        logo.close();
+    if (logo.is_open()){
+      while ( getline (logo,line) ){
+        cout << line << '\n';
       }
-
-      else cout << "Unable to open file";
-      return EXIT_SUCCESS;
+      logo.close();
     }
 
+    else cout << "Unable to open file";
+    return EXIT_SUCCESS;
+  }
 
-return 0;
+
+  return 0;
 }
